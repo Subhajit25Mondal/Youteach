8d1f36201e0e7d5618fd6dfe62a040ea1b835e08
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-04-28 01:04:07 +0530

    mw

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Contact YouTeach</title>
+  <link rel="icon" href="imgs/header.png">
+  <link rel="stylesheet" href="css/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/css/froala_blocks.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i">
+  <link rel="stylesheet" href="css/css/froala_editor.pkgd.min.css">
+  <link rel="stylesheet" href="css/css/froala_style.min.css">
+</head>
+<body>
+<section class="fdb-block bg-dark fp-active" style="background-image: url(imgs/blue.svg);" data-block-type="contacts" data-id="1">
+  <div class="container">
+    <div class="row text-center justify-content-center">
+      <div class="col-12 col-md-8 col-lg-7" style="z-index: 10000;"><h1>Contact Us</h1><h2>We love to hear from you!</h2></div>
+    </div>
+
+    <div class="row pt-4">
+      <div class="col-12" style="z-index: 10000;"><form><div class="row"><div class="col-12 col-md"><input type="text" class="form-control" placeholder="Name" value=""></div><div class="col-12 col-md mt-4 mt-md-0"><input type="text" class="form-control" placeholder="Email" value=""></div><div class="col-12 col-md mt-4 mt-md-0"><input type="text" class="form-control" placeholder="Phone (optional)" value=""></div></div><div class="row mt-4"><div class="col"><input type="email" class="form-control" placeholder="Subject" value=""></div></div><div class="row mt-4"><div class="col"><textarea class="form-control" name="message" rows="3" placeholder="How can we help?" value=""></textarea></div></div><div class="row mt-4"><div class="col text-center"><button class="btn btn-dark" type="submit">Submit</button></div></div></form></div>
+    </div>
+  </div>
+</section>
+
+</body>
+</html>

2cfbb27406182fe5af0f7f098ec87d5e0b97ecfd
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-05-05 17:22:47 +0530

    update mechanism complete

--- update/details.php
+++ update/details.php
@@ -1,4 +1,11 @@
-<?php $id = $_GET["id"]; ?>
+<?php 
+error_reporting(0);
+$id = $_GET["id"]; 
+include("../connect.php");
+$sqll = "SELECT * FROM inst WHERE inst_id=$id";
+$resultss = mysqli_query($con, $sqll);
+$roww = mysqli_fetch_assoc($resultss);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,19 +23,19 @@
             <div aria-disabled="false" class="fr-element fr-view" dir="auto"
             spellcheck="true">
             <h1>Update Details</h1>
-            <p>Please fill the required details in the fields below. <b>Every field is mandatory.</b></p>
+            <p>Please update the details in the fields below. <b>Every field is mandatory.</b></p>
             </div>       
     </div>
     <div class="jumbotron">
         <div class="container">
-            <form method="POST" action="details2.php">
+            <form method="POST" action="details.php">
                 <input type="text" class="form-control" name="id" value="<?php echo "$id" ?>" hidden>
                 <div class="row align-items-center">
                         <div class="col mt-4 fr-box" role="application" style="z-index: 10000;">
                           <div class="fr-wrapper" dir="auto">
                             <div aria-disabled="false" class="fr-element fr-view" dir="auto"
                               spellcheck="true">
-                              <p><input type="text" class="form-control" placeholder="Address" name="address" required></p>
+                              <p><input type="text" class="form-control" placeholder="Address" name="address" value="<?php echo "$roww[address]"; ?>" required></p>
                             </div>
                           </div>
                         </div>
@@ -38,25 +45,25 @@
                           <div class="fr-wrapper" dir="auto">
                             <div aria-disabled="false" class="fr-element fr-view" dir="auto"
                               spellcheck="true">
-                              <p><input type="text" class="form-control" placeholder="Phone no. (with country code)" name="phone" required></p>
+                              <p><input type="text" class="form-control" placeholder="Phone no. (with country code)" name="phone" value="<?php echo "$roww[phone]"; ?>" required></p>
                             </div>
                           </div>
                         </div>
                       </div>
                       <div class="row mt-4">
-                            <div class="col"><textarea class="form-control" rows="3" placeholder="About yourself" name="bio"></textarea></div>
+                            <div class="col"><textarea class="form-control" rows="3" placeholder="About yourself" name="bio" required><?php echo "$roww[bio]"; ?></textarea></div>
                         </div>
                         <div class="row align-items-center">
                                 <div class="col mt-4 fr-box" role="application" style="z-index: 10000;">
                                   <div class="fr-wrapper" dir="auto">
                                     <div aria-disabled="false" class="fr-element fr-view" dir="auto"
                                       spellcheck="true">
-                                      <p><input type="text" class="form-control" placeholder="Instructor Certificate Link" name="cert" required></p>
+                                      <p><input type="text" class="form-control" placeholder="Instructor Certificate Link" name="cert" value="<?php echo "$roww[certificate]"; ?>" required></p>
                                     </div>
                                   </div>
                                 </div>
                               </div>
-                <center><p><button class="btn btn-primary mt-4" type="submit" name="submit">Submit</button></p></center>
+                <center><p><button class="btn btn-primary mt-4" type="submit" name="submit">Update</button></p></center>
             </form>
         </div>
     </div>
@@ -74,7 +81,10 @@
     $results = mysqli_query($con, $sql);
   if($results){
   echo "<script>
-            window.location.href = \"done.html\";
+            alert(\"Details has been updated. Please refresh the dashboard to view changes.\")
+            window.history.back();
+            window.history.back();
+            location.reload(1);
             </script>";
   }
   else{
